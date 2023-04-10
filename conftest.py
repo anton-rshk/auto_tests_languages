f9abd000f2743da00511a3fae39c4e1a25fe85d2
unknown <anton.ir.2005@gmail.com>
2023-04-10 20:19:35 +0300

    Fixed bags

--- conftest.py
+++ conftest.py
@@ -2,30 +2,22 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.options import Options
 
-# для корректного отображения кириллицы в параметризаторах
 
-
-def pytest_make_parametrize_id(config, val): return repr(val)
-
-
-# добавляем параметр запуска тестов в командной строке(чем запускать, хромом или фаерфоксом) По умолчанию хром
 def pytest_addoption(parser):
-    # parser.addoption('--browser_name', action='store', default=None, help="Choose browser: chrome or firefox")
-    # Можно задать значение параметра по умолчанию,
-    # чтобы в командной строке не обязательно было указывать параметр --browser_name, например, так:
     parser.addoption('--language', action='store',
-                     default="en", help="Choose any language:")
+                     default=None, help="Choose any language:")
 
 
-# Запуск браузера(для каждой функции)
-# по умолчанию запускается для каждой функции
 @pytest.fixture(scope="function")
 def browser(request):
-    # получаем параметр командной строки browser_name
-    language = request.config.getoption("language")
-    Options().add_experimental_option(
-        'prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=Options())
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,12 +1,12 @@
 from selenium.webdriver.common.by import By
-
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_add_to_basket(browser):
     browser.get(link)
+    time.sleep(10)
     button = browser.find_element(
         By.CSS_SELECTOR, "#add_to_basket_form > button")
-    # "btn btn-lg btn-primary btn-add-to-basket")
 
     assert button
